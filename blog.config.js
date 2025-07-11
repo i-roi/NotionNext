537c7d81ea47a65280c52eb918820aae094b6dad
i-roi <e_suni@163.com>
2025-07-11 11:02:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,8 +16,8 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Nic', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '欲买桂花同载酒 终不似 少年游' // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'www.snyii.cn', // 网站地址
+  BIO: process.env.NEXT_PUBLIC_BIO || '欲买桂花同载酒 终不似 少年游', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://snyii.cn', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Nic', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || 'https://cdn.picui.cn/vip/2025/07/11/6870745728210.png', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
