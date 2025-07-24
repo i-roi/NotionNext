9b626b8b211c410508d864acfa9ebf0cc083d2db
i-roi <e_suni@163.com>
2025-07-24 16:38:44 +0800

    Update Header.js

--- themes/example/components/Header.js
+++ themes/example/components/Header.js
@@ -10,16 +10,7 @@ import { MenuList } from './MenuList'
 export const Header = props => {
   return (
     <header className='w-full px-6 bg-white  dark:bg-black relative z-20'>
-      //<div className='mx-auto max-w-4xl md:flex justify-between items-center'>
-        //<Link
-          //href='/'
-          //className='logo py-6 w-full text-center md:text-left md:w-auto text-gray-dark no-underline flex justify-center items-center'>
-          //{siteConfig('TITLE')}
-        //</Link>
-        //<div className='w-full md:w-auto text-center md:text-right'>
-          //{/* 右侧文字 */}
-        //</div>
-      //</div>
+      
 
 
       {/* 菜单 */}
